53d663b01afc19e494de8e1543e954ddaeb70a47
Tianning Li <51225681+lit26@users.noreply.github.com>
2020-07-23 17:26:59 -0700

    Delete webrequest.py

--- webrequest.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-def webScrap(url):
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) \
-                   AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
-    website = requests.get(url, headers=headers)
-    soup = BeautifulSoup(website.text, 'lxml')
-    return soup
