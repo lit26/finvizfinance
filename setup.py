1ac0c79a969751ed332067cdce6075aac65db2df
Tianning Li <ltianningli@gmail.com>
2020-07-28 16:07:49 -0700

    build package

---
+++ setup.py
@@ -0,0 +1,35 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.1.0'
+PACKAGE_NAME = 'finvizfinance'
+AUTHOR = 'Tianning Li'
+AUTHOR_EMAIL = 'ltianningli@gmail.com'
+URL = 'https://github.com/lit26/finvizfinance'
+
+LICENSE = 'The MIT License (MIT)'
+DESCRIPTION = 'Finviz Finance. Information downloader'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'pandas',
+      'datetime',
+      'requests',
+      'bs4'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )
