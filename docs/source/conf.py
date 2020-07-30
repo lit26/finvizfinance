798c25a0a9391db2981dbe91ea17bbe7ad93ad08
Tianning Li <ltianningli@gmail.com>
2020-07-29 19:35:47 -0700

    update documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,3 +59,6 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# setting master_doc
+master_doc = 'index'
