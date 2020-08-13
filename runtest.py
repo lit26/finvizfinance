eb71ca6d288e4f96cece90036dc1b530322d3b13
Tianning Li <ltianningli@gmail.com>
2020-08-13 04:52:56 -0700

    update test

--- runtest.py
+++ runtest.py
@@ -1,6 +1,8 @@
 from finvizfinance.quote import finvizfinance
 from finvizfinance.news import News
 from finvizfinance.insider import Insider
+from finvizfinance.forex import Forex
+from finvizfinance.crypto import Crypto
 from finvizfinance.screener.overview import Overview
 
 def test_finvizfinance_quote():
@@ -32,6 +34,16 @@ def test_screener_overview():
     df = foverview.ScreenerView()
     assert(df is not None)
 
+def test_finvizfinance_forex():
+    fforex = Forex()
+    df = fforex.performance()
+    assert (df is not None)
+
+def test_finvizfinance_crypto():
+    fcrypto = Crypto()
+    df = fcrypto.performance()
+    assert (df is not None)
+
 if __name__ == "__main__":
     test_finvizfinance_quote()
     print('Quote module test pass.')
@@ -41,3 +53,7 @@ if __name__ == "__main__":
     print('Insider module test pass.')
     test_screener_overview()
     print('Screener module test pass.')
+    test_finvizfinance_forex()
+    print('Forex module test pass.')
+    test_finvizfinance_crypto()
+    print('Crypto module test pass.')
