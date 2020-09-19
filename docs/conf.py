ea6a7b1875e919c223f7de6eca43d8d1a95c6de8
Tianning Li <ltianningli@gmail.com>
2020-09-19 15:17:57 -0700

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -30,10 +30,18 @@ release = '0.7'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc','sphinx_rtd_theme']
+# extensions = ['sphinx.ext.autodoc','sphinx_rtd_theme']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+]
+napoleon_google_docstring = True
+napoleon_use_param = False
+napoleon_use_ivar = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
