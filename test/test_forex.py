88d4c262a7f55d939f246f6e6a6569d8e483f93d
Tianning Li <ltianningli@gmail.com>
2021-12-05 20:40:03 -0800

    update test

--- test/test_forex.py
+++ test/test_forex.py
@@ -1,5 +1,26 @@
-def test_finvizfinance_forex():
-    from finvizfinance.forex import Forex
+import pytest
+from finvizfinance.forex import Forex
+
+
+def test_forex_performance_percentage():
     fforex = Forex()
     df = fforex.performance()
     assert (df is not None)
+
+
+def test_forex_performance_pips():
+    fforex = Forex()
+    df = fforex.performance(change='PIPS')
+    assert (df is not None)
+
+
+def test_forex_performance_error():
+    with pytest.raises(ValueError):
+        fforex = Forex()
+        fforex.performance(change='Dummy')
+
+
+def test_forex_chart_none():
+    fforex = Forex()
+    fforex.chart()
+
