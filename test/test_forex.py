4dad52b0a50515b765def388475f3e6ad0c4a7e0
Tianning Li <ltianningli@gmail.com>
2021-12-05 23:28:50 -0800

    add test

--- test/test_forex.py
+++ test/test_forex.py
@@ -1,5 +1,21 @@
+import pytest
+from finvizfinance.forex import Forex
+
 def test_finvizfinance_forex():
-    from finvizfinance.forex import Forex
     fforex = Forex()
     df = fforex.performance()
     assert (df is not None)
+
+
+def test_forex_performance_pips(mocker):
+    mocker.patch('finvizfinance.forex.scrapFunction', return_value="df")
+    fforex = Forex()
+    df = fforex.performance(change='PIPS')
+    assert (df is not None)
+
+
+def test_forex_performance_error():
+    with pytest.raises(ValueError):
+        fforex = Forex()
+        fforex.performance(change='Dummy')
+
